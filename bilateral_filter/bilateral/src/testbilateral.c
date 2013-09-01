c560c9b9aca67fcc729bacdbe73870c98385c48b
Gabriele Facciolo <gfacciol@gmail.com>
2013-09-01 04:33:03 +0200

    propose iio DOUBLE data type wrappers

--- bilateral_filter/bilateral/src/testbilateral.c
+++ bilateral_filter/bilateral/src/testbilateral.c
@@ -6,6 +6,23 @@
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 
+void iio_save_image_double_(char* filename_out, double*in,int w,int h){
+    int N=w*h;
+    float *tmp=malloc(N*sizeof(*tmp));
+    for (int i=0;i<N;i++) tmp[i] = in[i];
+    iio_save_image_float((char *)filename_out, tmp, w, h);
+    free(tmp);
+}
+
+double *iio_read_image_double_(const char *fname, int *w, int *h) {
+    float *tmp=iio_read_image_float(fname, w, h);
+    int N = *w * *h;
+    double *out=malloc(N*sizeof(*out));
+    for (int i=0;i<N;i++) out[i] = tmp[i];
+    free(tmp);
+    return out;
+}
+
 int main () {
     
     const char * filename_in = "../../data/Lena.pgm";
@@ -14,7 +31,7 @@ int main () {
     int w = 0;
     int h = 0;
     
-    data_t *img_in = iio_read_image_double(filename_in, &w, &h);  
+    data_t *img_in = iio_read_image_double_(filename_in, &w, &h);  
     
     data_t *img_out = bilateral(img_in, w, h, 25.6, 16.0);
     
@@ -30,7 +47,7 @@ int main () {
     cvReleaseImage(&image_out);
 
     
-//    iio_save_image_double((char *)filename_out, img_out, w, h);
+    iio_save_image_double_((char *)filename_out, img_out, w, h);
     
     free(img_in);
     free(img_out);
