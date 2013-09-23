d463d0b5d472048635933fa9ff71b19b269822c0
Martin Etchart <mrtn.etchart@gmail.com>
2013-09-22 23:17:26 -0300

    Adding symmetry to bilateral filter

--- bilateral_filter/bilateral/src/bilateral.c
+++ bilateral_filter/bilateral/src/bilateral.c
@@ -34,9 +34,17 @@ float s(float phi, float f, double sigma_r) {
     return exp(-0.5*powf((delta(phi,f)/sigma_r),2.0));
 }
 
+// prolong with last value at the boundary
+// returns the index of the 2d row major image
+unsigned int p_prolong(int nx, int ny, int x, int y) 
+{ 
+   x = (x < 0) ? 0 : ( (x>=nx) ? nx-1 : x );
+   y = (y < 0) ? 0 : ( (y>=ny) ? ny-1 : y );
+   return x+nx*y;
+}
 
 
-void bilateral(const float * f, float * h, int width, int height, double sigma_r, double sigma_d) {
+void bilateral_no_symmetry(const float * f, float * h, int width, int height, double sigma_r, double sigma_d) {
      
     int win = 2 * sigma_d;
     
@@ -53,6 +61,35 @@ void bilateral(const float * f, float * h, int width, int height, double sigma_r
                     
                     int eps[2] = {u, v};
                     int _eps = u + v * width;
+                    
+                    float cs = c(eps,x,sigma_d)*s(f[_eps],f[_x],sigma_r);
+                    b+=f[_eps]*cs;                  
+                    k+=cs;
+                }
+            }
+            
+            h[_x] = (1.0/k) * b;
+        }
+    }
+}
+
+void bilateral(const float * f, float * h, int width, int height, double sigma_r, double sigma_d) {
+     
+    int win = 2 * sigma_d;
+    
+    for (int j = 0; j < height ; j++) {
+        for (int i = 0; i < width ; i++) {
+            
+            int x[2] = {i,j};
+            int _x = p_prolong(width,height,i,j);
+            
+            float k = 0;
+            float b = 0;
+            for (int v = j - win; v < j + (win + 1); v++) {
+                for (int u = i - win; u < i + (win + 1); u++) {
+                    
+                    int eps[2] = {u, v};
+                    int _eps = p_prolong(width,height,u,v);
                     
                     float cs = c(eps,x,sigma_d)*s(f[_eps],f[_x],sigma_r);
                     b+=f[_eps]*cs;                  
